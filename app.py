a351ba61919b3b99724e837ec9a28809a725ead9
Anushkagarg05 <anushkagarg05@gmail.com>
2023-12-06 16:36:46 -0600

    remaining changes

--- app.py
+++ app.py
@@ -246,7 +246,11 @@ def singlestate():
     ]
 
     if (selected_state == 'Please select a state'):
-        return render_template('Filter.html', show_content=False, states=states, chart_view=chart_view, years=years, current_state=selected_state, current_compare_state=compare_state, plot_data={}, view=view, cc='', cc2='')
+        return render_template('Filter.html', show_content=False, states=states, chart_view=chart_view, 
+        years=years, current_state=selected_state, current_compare_state=compare_state, plot_data={}, 
+        view=view, cc='', cc2=''State = dataset1["State"].tolist(), SAT_mean = dataset1["SAT Mean"].tolist(),
+        CC = dataset1["Total Completion (%)"].tolist(), Population= dataset1["Population"].tolist(),
+        Region = dataset1["Region"].tolist())
     else:
     
         state_data = dataset[dataset['State'] == selected_state]
@@ -309,7 +313,11 @@ def singlestate():
             plot_data['math'] = list(state_data['Mathematics'])
 
         correlation_coefficient = str(round(correlation_coefficient, 2))
-        return render_template('Filter.html', show_content=True, states=states, chart_view=chart_view, years=years, current_state=selected_state, current_compare_state=compare_state, plot_data=plot_data, view=view, cc=correlation_coefficient, cc2=correlation_coefficient_compare)
+        return render_template('Filter.html', show_content=True, states=states, chart_view=chart_view, years=years, 
+        current_state=selected_state, current_compare_state=compare_state, plot_data=plot_data, view=view, 
+        cc=correlation_coefficient, cc2=correlation_coefficient_compareState = dataset1["State"].tolist(), SAT_mean = dataset1["SAT Mean"].tolist(),
+        CC = dataset1["Total Completion (%)"].tolist(), Population= dataset1["Population"].tolist(),
+        Region = dataset1["Region"].tolist())
 
 @app.route('/process_saved', methods=['GET'])
 def process_saved_view():
